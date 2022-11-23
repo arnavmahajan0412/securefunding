0d04056ce90684bc314f86282bd9223847f72468
Hassaan Khan <khassaan01@gmail.com>
2022-11-23 20:45:49 +0530

    first commit

---
+++ contracts/createproject.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract createproject{
+    string public projectName;
+    string public projectDescription;
+    uint256 public targetAmount;
+    uint256 public minContribution;
+    uint256 public raisedAmount;
+    
+}
